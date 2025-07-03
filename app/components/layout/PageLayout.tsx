9cba4ead60c26dd446bf9277eaa0d143bfc7c0f3
IngMyr <ingrid.myrann@gmail.com>
2025-07-03 09:58:07 +0200

    Start på nytt med Vite + React + TypeScript

--- app/components/layout/PageLayout.tsx
+++
@@ -1,3 +0,0 @@
-export const PageLayout = ({ children }: { children: React.ReactNode }) => {
-    return <main className="flex items-center justify-center pt-16 pb-4">{children}</main>;
-};
--- app/routes/home.tsx
+++
@@ -1,6 +0,0 @@
-import type { Route } from './+types/home';
-import { Welcome } from '~/components/welcome';
-
-export default function Home() {
-    return <Welcome />;
-}
