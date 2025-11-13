dbcd97028becb4415916803aa8f8332a63b02722
IngMyr <ingrid.myrann@gmail.com>
2025-11-13 14:28:35 +0100

    Add basename

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,12 @@
 import type { Route } from './+types/home';
 import { Welcome } from '~/components/welcome';
+const baseUrl = import.meta.env.BASE_URL;
 
-export default function Home() {
+export async function clientLoader() {
+    return baseUrl;
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+    console.log(loaderData);
     return <Welcome />;
 }
