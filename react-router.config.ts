b4e8ab398452267c9fbb6c1f5cb8050a1e823204
IngMyr <ingrid.myrann@gmail.com>
2025-11-18 08:55:51 +0100

    Test trailing backspace

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,7 @@
 import type { Config } from "@react-router/dev/config";
+const BASE_PATH = process.env.BASE_PATH
 
 export default {
-  // Config options...
-    basename: process.env.BASE_PATH,
-  // Server-side render by default, to enable SPA mode set this to `false`
+  basename: BASE_PATH,
   ssr: true,
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+const BASE_PATH = process.env.BASE_PATH
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-    base: process.env.BASE_PATH,
+  base: BASE_PATH,
 });
