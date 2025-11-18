731b6ba572243aa3b9186a25fc2e0613cef9d2b0
IngMyr <ingrid.myrann@gmail.com>
2025-11-18 14:25:56 +0100

    test empty base

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcssVite from "@tailwindcss/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
-
-    console.log("=== defineConfig env ====", env.BASE_PATH);
-
     return {
         plugins: [tailwindcssVite(), reactRouter(), tsconfigPaths()],
-        base: `${env.BASE_PATH}/`,
+        base: '',
     }
 });
 
