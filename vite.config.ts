e5b27160d5b0fe24d5d56c26e3761c8db553f8fb
IngMyr <ingrid.myrann@gmail.com>
2025-11-21 12:11:17 +0100

    Fallback to empty base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
 
-    const basePath = `${process.env.BASE_PATH ?? ''}/`
+    const basePath = process.env.BASE_PATH ? `${process.env.BASE_PATH ?? ''}/` : ''
     console.log("=== basePath", basePath);
 
     return {
