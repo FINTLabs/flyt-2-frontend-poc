2e7f82698b0267a73fd1d413cfb51825a8d519d3
Ingrid W. Myrann <ingrid.myrann@gmail.com>
2025-12-03 10:19:01 +0100

    FFS-1593-deployment-config-part2 (#12)
    * fix ingress path
    
    * Add basename
    
    * Test trailing backspace
    
    * remove resource specs
    
    * remove resource specs
    
    * change replace for url
    
    * Test css import
    
    * Fix imports
    
    * vite base test
    
    * test vite config tailwinf path
    
    * loadenv in vite config
    
    * test empty base
    
    * Test some more vite config stuff
    
    * Change vite base again
    
    * New vite config change
    
    * Build project in workflow script
    
    * Fallback to empty base

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "@react-router/dev/config";
+const BASE_PATH = process.env.BASE_PATH
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
+  basename: BASE_PATH,
   ssr: true,
 } satisfies Config;
