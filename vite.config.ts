1ccfc50dc3d33d6e65451a5d29aa5ab5a9af024c
IngMyr <ingrid.myrann@gmail.com>
2026-07-07 11:53:47 +0200

    Remove deprecated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcssVite from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({mode}) => {
     const basePath = process.env.BASE_PATH ? `${process.env.BASE_PATH ?? ''}/` : ''
 
     return {
-        plugins: [tailwindcssVite(), reactRouter(), tsconfigPaths()],
+        plugins: [tailwindcssVite(), reactRouter()],
+        resolve: {
+            tsconfigPaths: true,
+        },
         base: basePath,
         server: {
             port: mode === 'production' ? 8000 : 3000,
