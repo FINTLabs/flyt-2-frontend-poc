905f4808074e237c7b9ae7a03c8226cd4ab09643
IngMyr <ingrid.myrann@gmail.com>
2026-07-07 12:02:58 +0200

    React-router 8

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,11 +4,5 @@ const BASE_PATH = process.env.BASE_PATH
 export default {
     basename: BASE_PATH,
     ssr: false,
-    future: {
-        v8_middleware: false,
-        v8_splitRouteModules: false,
-        v8_viteEnvironmentApi: false,
-        v8_passThroughRequests: false,
-        v8_trailingSlashAwareDataRequests: false,
-    },
+
 } satisfies Config;
