72c8574c4e1af96b50d2a4346f305f6c5830f817
IngMyr <ingrid.myrann@gmail.com>
2026-07-09 10:41:45 +0200

    Turn off api fututre flagg

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,11 +2,11 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
     basename: process.env.BASE_PATH || "",
-    ssr: false,
+    ssr: true,
     future: {
         v8_middleware: true,
         v8_splitRouteModules: true,
-        v8_viteEnvironmentApi: true,
+        v8_viteEnvironmentApi: false,
         v8_passThroughRequests: true,
         v8_trailingSlashAwareDataRequests: true,
     },
